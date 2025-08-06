c91bb1aef09cd04d297f24fbf071ced3978fd6e8
Matheus Bertoldo <matheusdeoliveiracjt@gmail.com>
2025-08-06 20:17:07 -0300

    aula 06 - 08

---
+++ Javascript/01.01/js-01.01.js
@@ -0,0 +1,10 @@
+//Tipos de funções
+
+//Função simples 
+
+function showMessage(){
+    document.write("<h2>Olá! Bem-vindo! Essa é sua primeira lição</h2>")
+}
+
+//Chamando a função simples
+showMessage()
