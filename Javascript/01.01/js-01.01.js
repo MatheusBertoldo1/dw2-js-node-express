67c4a3fec13969f3933df75c53ab6b3061460a0b
Matheus Bertoldo <matheusdeoliveiracjt@gmail.com>
2025-08-07 18:19:25 -0300

    aula 07-08

--- Javascript/01.01/js-01.01.js
+++ Javascript/01.01/js-01.01.js
@@ -1,10 +1,25 @@
 //Tipos de funções
 
-//Função simples 
+//Função simples
+/*
+VAR: Evitar o uso 
+LET: Utilize quando for necessário reatribuir o valor da variável
+CONST: Define uma variável com valor imutável
+*/
 
-function showMessage(){
-    document.write("<h2>Olá! Bem-vindo! Essa é sua primeira lição</h2>")
+const message = "<h2>Olá! Bem-vindo! Essa é sua primeira lição</h2>";
+function showMessage() {
+  document.write(message);
 }
 
 //Chamando a função simples
-showMessage()
+showMessage();
+
+//Função com parâmetro
+const user = "Matheus";
+
+function userMessage(user){
+    document.write(`<h3>O que deseja fazer hoje ${user} <h3>`)
+}
+
+userMessage(user)
