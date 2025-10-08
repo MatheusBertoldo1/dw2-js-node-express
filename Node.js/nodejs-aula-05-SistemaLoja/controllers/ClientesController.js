872324ec15dc3baff31fd394e0855dec94a2a202
Matheus Bertoldo <matheusdeoliveiracjt@gmail.com>
2025-10-08 20:46:33 -0300

    alterar dados

--- Node.js/nodejs-aula-05-SistemaLoja/controllers/ClientesController.js
+++ Node.js/nodejs-aula-05-SistemaLoja/controllers/ClientesController.js
@@ -31,33 +31,33 @@ router.post("/clientes/new", (req,res) =>{
   })
 });
 
-export default router;
+// ROTA DE EXCLUSÃO DE DADOS
+// :id é um parâmetro obrigatório
+router.get("/clientes/delete/:id", (req, res) => {
+  const id = req.params.id
 
-/* 
- const clientes = [
-    {
-      nome: "Ana Silva",
-      cpf: "123.456.789-00",
-      endereco:
-        "Rua das Flores, 123, Bairro Jardim Primavera, Cidade Felicidade, Estado do Sonho, CEP: 12345-678",
-    },
-    {
-      nome: "Pedro Almeida",
-      cpf: "987.654.321-00",
-      endereco:
-        "Avenida Central, 456, Bairro Centro, Cidade Nova, Estado da Esperança, CEP: 98765-432",
-    },
-    {
-      nome: "Marina Oliveira",
-      cpf: "456.789.123-00",
-      endereco:
-        "Travessa dos Sonhos, 789, Bairro Vista Linda, Cidade Sol Nascente, Estado da Harmonia, CEP: 54321-987",
-    },
-    {
-      nome: "Rafael Santos",
-      cpf: "321.654.987-00",
-      endereco:
-        "Praça da Amizade, 321, Bairro Bela Vista, Cidade Alegria, Estado da Serenidade, CEP: 87654-321",
-    },
-  ];
-*/
+  // .destroy -> exclui um registro do banco
+  Clientes.destroy({
+    where:{
+      id: id
+    }
+  }).then(() => {
+    res.redirect("/clientes")
+  }).catch(error => {
+    console.log(error)
+  })
+})
+
+// ROTA DE EDIÇÃO DE CLIENTES
+router.get("/clientes/edit/:id", (req, res) => {
+  const id = req.params.id
+
+  //Buscando o cliente pelo ID
+  Clientes.findByPk(id).then(cliente => {
+    res.render("clientesEdit", {
+      cliente: cliente
+    })
+  })
+})
+
+export default router;
